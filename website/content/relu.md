7a4911fc657b87cb11b2980bff6bebe22227b022
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2021-06-01 23:07:12 +0200

    TEMP

---
+++ website/content/relu.md
@@ -0,0 +1,9 @@
+Title: Rectified Linear Unit (ReLU)
+Date: 2010-12-03 10:20
+Category: Machine Learning Terms
+Slug: relu
+Summary: A simple function of `x` returning 0 if `x < 0` or `x` otherwise.
+
+A simple function of `x` returning 0 if `x < 0` or `x` otherwise.
+
+ReLU is a popular activation function in deep neural networks because it is fast to compute and helps avoid the vanishing gradients problem.
--- website/pelicanconf.py
+++ website/pelicanconf.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = 'Vladimir Haltakov'
-SITENAME = 'Suboptimal'
-SITEURL = ''
+AUTHOR = "Vladimir Haltakov"
+SITENAME = "Suboptimal"
+SITEURL = ""
+PATH = "content"
+TIMEZONE = "Europe/Paris"
+DEFAULT_LANG = "en"
 
-PATH = 'content'
-
-TIMEZONE = 'Europe/Paris'
-
-DEFAULT_LANG = 'en'
+# Set the theme to minimal
+THEME = "themes/minimal/"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -17,18 +17,7 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
